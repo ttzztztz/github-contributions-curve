84b43a909858fb8ea543395ec9a7132adbe1d587
ttzztztz <ttzztztz@outlook.com>
2021-06-17 17:32:42 +0800

    chore: update dependencies

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-        net: "empty",
-        tls: "empty",
-        child_process: "empty"
-      };
-    }
-
     return config;
   },
 };
